7211a497d340db1a18aa0571cc22a78bc39e7eef
jamesb <experimentallearning0@gmail.com>
2022-11-28 09:53:31 +0000

    Add example style settings and command'

--- src/widgets/index.tsx
+++ src/widgets/index.tsx
@@ -1,31 +1,72 @@
-import { declareIndexPlugin, ReactRNPlugin, WidgetLocation } from '@remnote/plugin-sdk';
+import { declareIndexPlugin, ReactRNPlugin } from "@remnote/plugin-sdk";
 
+/**
+ * Simple example snippet plugin which shows how to:
+ * - Register style settings
+ * - Register Custom CSS
+ * - Register a command
+ *
+ * How to Use:
+ * - Tag a Rem with ##Plugin Style, or use the /Add Plugin Style command on a Rem
+ * - The Rem will be styled with the CSS defined in the plugin
+ */
 async function onActivate(plugin: ReactRNPlugin) {
-  // Register settings
+  // Register a setting to change the Rem's text color
   await plugin.settings.registerStringSetting({
-    id: 'name',
-    title: 'What is your Name?',
-    defaultValue: 'Bob',
+    id: "color",
+    title: "Text Color (hex)",
+    description: "Provide a hex color for the text",
+    defaultValue: "#ff0000",
   });
 
-  await plugin.settings.registerBooleanSetting({
-    id: 'pizza',
-    title: 'Do you like pizza?',
-    defaultValue: true,
+  // Each time the setting changes, re-register the text color css.
+  plugin.track(async (reactivePlugin) => {
+    const color = await reactivePlugin.settings.getSetting("color");
+    await reactivePlugin.app.registerCSS(
+      "color",
+      `[data-rem-tags~="plugin-style"] { color: ${color}; }`
+    );
   });
 
-  await plugin.settings.registerNumberSetting({
-    id: 'favorite-number',
-    title: 'What is your favorite number?',
-    defaultValue: 42,
+  // Register a setting to change the Rem's margin
+  await plugin.settings.registerStringSetting({
+    id: "margin",
+    title: "Rem Margin (px)",
+    description: "Provide a margin for the Rem",
+    defaultValue: "10",
   });
 
-  // A command that inserts text into the editor if focused.
+  // Each time the margin setting changes, re-register the margin css.
+  plugin.track(async (reactivePlugin) => {
+    const margin = await reactivePlugin.settings.getSetting<string>("margin");
+    try {
+      const marginAsNumber = Number.parseInt(margin);
+      await reactivePlugin.app.registerCSS(
+        "margin",
+        `[data-rem-tags~="plugin-style"] { margin: ${marginAsNumber}px; }`
+      );
+    } catch {}
+  });
+
+  // A command that adds a style tag to the current focused Rem.
   await plugin.app.registerCommand({
-    id: 'editor-command',
-    name: 'Editor Command',
+    id: "add-style-tag",
+    name: "Add Plugin Style",
+    description: "Add a style tag to the current focused Rem",
     action: async () => {
-      plugin.editor.insertPlainText('Hello World!');
+      const focusedRem = await plugin.focus.getFocusedRem();
+      if (!focusedRem) {
+        return;
+      }
+      let tag = await plugin.rem.findByName(["Plugin Style"], null);
+      if (!tag) {
+        tag = await plugin.rem.createRem();
+        if (!tag) {
+          return;
+        }
+        await tag.setText(["Plugin Style"]);
+      }
+      await focusedRem.addTag(tag);
     },
   });
 }
